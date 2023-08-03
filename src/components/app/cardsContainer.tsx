971f8f894a993403c4082c070a15d4684a12a044
Meerzulee <meerzulee@gmail.com>
2023-08-03 16:11:22 +0600

    fix responsiveness

--- src/components/app/cardsContainer.tsx
+++ src/components/app/cardsContainer.tsx
@@ -12,7 +12,7 @@ export function CardsContainer({ children, title }: {
             <h4 className="font-semibold">{title}</h4>
             <Separator className="my-4 h-[2px]" />
 
-            <div className='flex justify-between max-[1600px]:flex-wrap  max-[1600px]:gap-5 max-[1600px]:justify-center  mt-3 mb-6'>
+            <div className='flex justify-between max-[1600px]:flex-wrap max-[1600px]:gap-6 max-[1600px]:justify-center  mt-3 mb-6'>
                 {children}
             </div>
         </div >
--- src/components/section/search.tsx
+++ src/components/section/search.tsx
@@ -9,7 +9,7 @@ export function SectionSearch() {
     return (
         <div className=''>
             {/* search */}
-            <div className='flex w-full gap-x-8'>
+            <div className='flex w-full gap-x-8 max-[1600px]:max-w-5xl mx-auto'>
                 <div className='flex flex-1 w-full border '>
                     <Input type="text" className='z-10 w-full px-3 py-2 text-base text-gray-600 placeholder-gray-300 rounded-md rounded-tr-none rounded-br-none ' placeholder='Enter Keywords, SPU, SKU' />
                     <SelectCategory className="text-gray-400 bg-gray-100 rounded-tl-none rounded-bl-none " />
